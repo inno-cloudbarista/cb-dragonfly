0e56b07c759c25bc5665622b48538b65d03d52a8
Jin-Whee-Park <best888810@naver.com>
2021-03-25 12:05:06 +0900

    [CB-Dragonfly] MCIS 모니터링 에이전트 설치 및 삭제 스크립트 내부 명칭 수정

--- file/uninstall_mcis_script.sh
+++ file/uninstall_mcis_script.sh
@@ -1,30 +1,30 @@
 
 #!/bin/bash
 
-echo "[CB-Milkyway: Start to Delete Milkyway]"
+echo "[CB-MCIS-Monitoring Dependent Component: Start to Delete CB-MCIS-Monitoring Dependent Component]"
 
-echo "[CB-Milkyway: UnInstall sysbench]"
+echo "[CB-MCIS-Monitoring Dependent Component: UnInstall sysbench]"
 sudo apt-get purge -y update
 sudo apt-get purge -y install sysbench
 
-echo "[CB-Milkyway: UnInstall Ping]"
+echo "[CB-MCIS-Monitoring Dependent Component: UnInstall Ping]"
 sudo apt-get purge -y iputils-ping
 
-echo "[CB-Milkyway: UnInstall debconf-utils]"
+echo "[CB-MCIS-Monitoring Dependent Component: UnInstall debconf-utils]"
 sudo apt-get purge -y debconf-utils
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password psetri1234ak'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password psetri1234ak'
 
-echo "[CB-Milkyway: UnInstall MySQL]"
+echo "[CB-MCIS-Monitoring Dependent Component: UnInstall MySQL]"
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y purge mysql-server
 
-echo "[CB-Milkyway: Generate dump tables for evaluation]"
+echo "[CB-MCIS-Monitoring Dependent Component: Generate dump tables for evaluation]"
 
 mysql -u root -ppsetri1234ak -e "CREATE DATABASE sysbench;"
 mysql -u root -ppsetri1234ak -e "CREATE USER 'sysbench'@'localhost' IDENTIFIED BY 'psetri1234ak';"
 mysql -u root -ppsetri1234ak -e "GRANT ALL PRIVILEGES ON *.* TO 'sysbench'@'localhost' IDENTIFIED  BY 'psetri1234ak';"
 
-echo "[CB-Milkyway: Deletion is done]"
+echo "[CB-MCIS-Monitoring Dependent Component: Deletion is done]"
 
 
 
